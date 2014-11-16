a3606c48dc44ac3cd750ce50f0f6e8e8a17ea8e8
Hawkrex <Hawkrex50@gmail.com>
2014-11-16 01:01:50 +0100

    Text integrated

--- PixelQuizz/Buzzer/src/main.cpp
+++ PixelQuizz/Buzzer/src/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML lol!");
+    sf::RenderWindow window(sf::VideoMode(200, 400), "SFML lol!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    shape.move(0f, 200.f)
 
     sf::Font font;
-    if(!font.loadFromFile("data/arial.ttf"))
-    {
+    sf::Text text("Attente", font);
+    text.setColor(sf::Color::Red);
+    try
+        if(!font.loadFromFile("data/arial.ttf"))
+            throw string("Error loading arial font");
+    catch(string const& chaine)
+        cerr << chaine << endl;
 
-    }
+    sf::TcpSocket socket;
+    sf::Socket::Status status = socket.connect("www.pixelspray.fr", 53000);
+    try
+        if(status != sf::Socket::Done)
+            throw string("Error network");
+    catch(string const& chaine)
+        cerr << chaine << endl;
+
 
     while (window.isOpen())
     {
@@ -23,11 +37,16 @@ int main()
 
             if(event.type == sf::Event::KeyPressed)
                 if(event.key.code == sf::Keyboard::Space)
+                {
                     shape.setFillColor(sf::Color::Yellow);
+                    text.setString("Go !!!");
+                    text.setColor(sf::Color::Green);
+                }
         }
 
         window.clear();
         window.draw(shape);
+        window.draw(text);
         window.display();
     }
 
